55eb1aeefc0224771a24df99ca1e2ec43cd79d9b
Marco Aurélio Mendes <68308754+marcomendesbidula@users.noreply.github.com>
2022-06-14 19:17:54 -0300

    feito a matriz do tabuleiro do jogo

---
+++ jogoDaVelha.py
@@ -0,0 +1,14 @@
+import os
+import random
+import colorama import Fore, Back, Style
+
+jogaNovamente = "s"
+jogadas = 0
+quemJoga = 1
+maxJogadas=9
+vit = "n"
+velha = [
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "]
+]
