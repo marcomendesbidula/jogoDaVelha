10267aa97445a03be91bffac6c4444da75b192f0
Marco Aurélio Mendes <68308754+marcomendesbidula@users.noreply.github.com>
2022-06-16 11:16:38 -0300

    feita a parte lógica do jogo

--- jogoDaVelha.py
+++ jogoDaVelha.py
@@ -1,6 +1,6 @@
 import os
 import random
-import colorama import Fore, Back, Style
+from colorama import Fore, Back, Style
 
 jogaNovamente = "s"
 jogadas = 0
@@ -12,3 +12,18 @@ velha = [
     [" ", " ", " "],
     [" ", " ", " "]
 ]
+
+def tela():
+    global velha
+    global jogadas
+    os.system("cls")
+    print("    0   1   2")
+    print("0:  " + velha[0][0] + " | " + velha[0][1] + " | " + velha[0][2])
+    print("   -----------")
+    print("1:  " + velha[1][0] + " | " + velha[1][1] + " | " + velha[1][2])
+    print("   -----------")
+    print("2:  " + velha[2][0] + " | " + velha[2][1] + " | " + velha[2][2])
+    print("Jogadas:" + Fore.GREEN + str(jogadas) + Fore.RESET)
+    
+while True:
+    tela()    
