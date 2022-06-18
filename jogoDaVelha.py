97178783e333af1aed1b43f4c01f73e684d05877
Marco Aurélio Mendes <68308754+marcomendesbidula@users.noreply.github.com>
2022-06-18 15:14:56 -0300

    implementado a função verificar vitória

--- jogoDaVelha.py
+++ jogoDaVelha.py
@@ -4,7 +4,7 @@ from colorama import Fore, Back, Style
 
 jogaNovamente = "s"
 jogadas = 0
-quemJoga = 1
+quemJoga = 1 #1 = Jogador - 2 = Computador  
 maxJogadas=9
 vit = "n"
 velha = [
@@ -25,5 +25,106 @@ def tela():
     print("2:  " + velha[2][0] + " | " + velha[2][1] + " | " + velha[2][2])
     print("Jogadas:" + Fore.GREEN + str(jogadas) + Fore.RESET)
     
+def jogadorJoga():
+    global jogadas
+    global quemJoga
+    global maxJogadas
+    if quemJoga==1 and jogadas<maxJogadas:
+      
+        try:
+            l=int(input("Linha..: "))
+            c=int(input("Coluna.: "))
+
+            while velha[l][c]!=" ":
+                l=int(input("Linha..: "))
+                c=int(input("Coluna.: "))
+            velha[l][c]="X"
+            quemJoga=2
+            jogadas+=2
+        except:
+            print("Jogada inválida") 
+            os.sistem("Pause")  
+            #vit"n"
+                 
+def cpuJoga():
+    global jogadas
+    global quemJoga
+    global maxJogadas
+    if quemJoga==2 and jogadas<maxJogadas:
+        l=random.randrange(0,3)
+        c=random.randrange(0,3)
+        while velha[l][c]!=" ":
+            l=random.randrange(0,3)
+            c=random.randrange(0,3)
+        velha[l][c]="O"
+        jogadas+=1
+        quemJoga=2
+
+def verificarVitoria():
+    global velha
+    vitoria="n"
+    simbolos=["X","O"]
+    for s in simbolos:
+        vitoria = "n" 
+        #Verificar linhas
+        il=ic=0
+        while il<3:
+            soma=0
+            ic=0
+            while ic<3:
+                if(velha[il][ic]==s):
+                    soma+=1
+                ic+=1           
+            if(soma==3):
+                vitoria= s
+                break
+            il+=1
+        if(vitoria!="n"):
+            break
+
+        #Verificar colounas
+        il=ic=0
+        while ic<3:
+            soma=0
+            il=0
+            while il<3:
+                if(velha[il][ic]==s):
+                    soma+=1
+                il+=1           
+            if(soma==3):
+                vitoria= s
+                break
+            ic+=1
+        if(vitoria!="n"):
+            break
+
+        #Verifica diagonal 1
+        soma=0
+        idiag=0
+        while idiag<3:
+            if(velha[idiag][idiag]==s):
+                soma+=1
+            idiag+=1
+        if(soma==3):
+            vitoria=s
+            break        
+
+        #Verifica diagonal 1
+        soma=0
+        idiagl=0
+        idiagc=2
+        while idiagc<3:
+            if(velha[idiagl][idiagc]==s):
+                soma+=1
+            idiagl+=1
+            idiagc-=1
+        if(soma==3):
+            vitoria=s
+            break   
+    return vitoria    
+
+
 while True:
-    tela()    
+    tela()
+    jogadorJoga()
+    cpuJoga()
